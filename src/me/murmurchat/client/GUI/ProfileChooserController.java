c500d0bd290e272785eb196a7572d32f9a40ed1a
BobsCandyCanes <grantmatthews28@yahoo.com>
2016-03-27 00:46:32 -0500

    Added validation for the .profile file

--- src/me/murmurchat/client/GUI/ProfileChooserController.java
+++ src/me/murmurchat/client/GUI/ProfileChooserController.java
@@ -13,7 +13,14 @@ import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class ProfileChooserController
 {
@@ -27,10 +34,15 @@ public class ProfileChooserController
     private JFXButton browseButton;
     
     @FXML
+    private JFXButton newProfileButton;
+    
+    @FXML
     private JFXButton doneButton;
 
     @FXML
     private JFXTextField fileDirectory;
+    
+    private File selectedFile;
     
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() 
@@ -47,15 +59,11 @@ public class ProfileChooserController
             	FileChooser fileChooser = new FileChooser();
             	FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Profiles (*.profile)", "*.profile");
             	fileChooser.getExtensionFilters().add(extFilter);
-            	File selectedFile = fileChooser.showOpenDialog(null);
+            	selectedFile = fileChooser.showOpenDialog(null);
             	 
             	if (selectedFile != null) 
             	{
             		fileDirectory.setText(selectedFile.getPath());
-            		
-            		Murmur.crypt = new Crypt(selectedFile.getPath());
-            		
-            		Murmur.serverHandler.start();
             	}
             }
         });
@@ -65,7 +73,23 @@ public class ProfileChooserController
             @Override
             public void handle(ActionEvent event) 
             {
-                GUI.launchMainWindow();
+            	if(selectedFile != null && fileDirectory.getText().endsWith(".profile"))
+            	{
+            		Murmur.crypt = new Crypt(selectedFile.getPath());
+        		
+        			Murmur.serverHandler.start();
+        			
+        			GUI.launchMainWindow();
+            	}
+            	else
+            	{
+            		Alert alert = new Alert(AlertType.WARNING);
+            		alert.setTitle("Invalid Profile");
+            		alert.setHeaderText(null);
+            		alert.setContentText("Please select a valid profile");
+
+            		alert.showAndWait();
+            	}
             }
         });
     }
