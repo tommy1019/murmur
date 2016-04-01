0b6392ec9a848d6ae464b9d30c9ea0b2183595cc
BobsCandyCanes <grantmatthews28@yahoo.com>
2016-04-01 01:14:24 -0500

    Added functionality to done button

--- src/me/murmurchat/client/GUI/UserInfoDialog.java
+++ src/me/murmurchat/client/GUI/UserInfoDialog.java
@@ -2,9 +2,20 @@ package me.murmurchat.client.GUI;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import me.murmurchat.client.AccountDatabase;
+import me.murmurchat.client.Murmur;
+import me.murmurchat.client.Profile;
+import me.murmurchat.client.ServerHandler;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 public class UserInfoDialog 
 {
@@ -23,6 +34,28 @@ public class UserInfoDialog
     @FXML
     void initialize() 
     {
+        doneButton.setOnAction(new EventHandler<ActionEvent>() 
+        {
+            @Override
+            public void handle(ActionEvent event) 
+            {
+            	String displayName = displayNameField.getText();
+            	
+            	if(displayName != null)
+            	{
+            		Murmur.accountDatabase = new AccountDatabase(displayName);
+            		Murmur.serverHandler.notify();
+            	}
+            	else
+            	{
+            		Alert alert = new Alert(AlertType.WARNING);
+            		alert.setTitle("Invalid Display Name");
+            		alert.setHeaderText(null);
+            		alert.setContentText("Please enter a valid display name");
 
+            		alert.showAndWait();
+            	}
+            }
+        });
     }
 }
